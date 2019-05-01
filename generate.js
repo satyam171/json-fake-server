20a3f6a28bd7e6c6ac0aad6c5e105bc2549c5f34
Satyam Tiwari <satyamtiwari171@gmail.com>
2019-05-01 20:34:17 +0530

    Fake server

---
+++ generate.js
@@ -0,0 +1,19 @@
+var faker = require('faker'); 
+
+module.exports = () => {
+    const data = { products: [] }
+    // Create 1000 products
+    for (let i = 0; i < 1000; i++) {
+      data.products.push({ 
+        id: i, 
+        productName: faker.commerce.productName(),
+        color: faker.commerce.color(),  
+        price: faker.commerce.price(), 
+        productAdjective: faker.commerce.productAdjective(), 
+        productMaterial: faker.commerce.productMaterial(), 
+        image: faker.image.fashion()
+        })
+    }
+    return data; 
+  }
+  
